b1857a1a0d8338abcd7ba1e09a26c728b467a1c9
ginsanwang <ginsanwang@tencent.com>
2018-08-06 11:15:16 +0800

    优化目录结构

--- pixidemo_pic/game.js
+++
@@ -1,4 +0,0 @@
-
-import Main from './js/main'
-
-new Main()
--- threejsdemo/main.js
+++
@@ -1,5 +0,0 @@
-var GameGlobal = this;
-var window = this;
-var demoName = "adapterDemo";
-this.pixelRatio = BK.Director.screenPixelSize.width/BK.Director.renderSize.width;
-BK.Script.loadlib("GameRes://entry.js");
