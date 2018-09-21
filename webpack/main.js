832403e9729106cb0fdec3f3289af25fa34d0531
curiezhu <curiezhu@tencent.com>
2018-09-21 18:04:38 +0800

    添加配置
    添加配置

--- webpack/main.js
+++ webpack/main.js
@@ -7,7 +7,9 @@ window["sharedCanvas"] = {}
 window.global=this;
 var console = {
     log: function (msg) {
-        BK.Script.log(2, 2, "console log: " + contact(arguments))
+        if(window.LOG_MODE_DEBUG) {
+            BK.Script.log(2, 2, "console log: " + contact(arguments))
+        }
     },
     warn:function (msg) {
         BK.Script.log(3, 3, "console warn: " + contact(arguments))
@@ -37,10 +39,31 @@ function contact(list){
     }
     return result
 };
-if(typeof window.EGRET_GAME === 'undefined'){
+
+
+//config
+window.LOG_MODE_DEBUG = true;
+window.ENGINE_NAME = "";
+var configPath = "GameRes://gameConfig.json";
+if(BK.FileUtil.isFileExist(configPath)){
+    const LOG_MODE = "debug";
+    const ENGINE = "engine";
+    var configList = JSON.parse(BK.FileUtil.readFile(configPath).readAsString());
+    if(configList){
+        window.LOG_MODE_DEBUG = !!configList[LOG_MODE];
+        if(window.LOG_MODE_DEBUG){
+            console.log("config :"+JSON.stringify(configList));
+        }
+        window.ENGINE_NAME = configList[ENGINE];
+    }
+}else {
+    console.error("config not found");
+}
+
+if(window.ENGINE_NAME  === 'egret'){
     window.promise = {};
 }
-BK.Script.logToConsole = 1;
+BK.Script.logToConsole = window.LOG_MODE_DEBUG ? 1 : 0;
 BK.Script.loadlib("GameRes://qqPlayCore.js");
 BK.Script.loadlib("GameRes://brick_wxadapter.js");
 BK.Script.loadlib("GameRes://promise.js");
